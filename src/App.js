3f4be79baa6d6e3227784b1afecc598908c64cae
aniskhoja <anis.khojaa@gmail.com>
2022-04-06 01:08:28 -0700

    added filter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, React } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import axios from 'axios';
-import { RadioButton } from 'primereact/radiobutton';
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { InputText } from 'primereact/inputtext';
+import { Dropdown } from 'primereact/dropdown';
+import { MultiSelect } from 'primereact/multiselect';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
@@ -10,43 +13,117 @@ import "primeicons/primeicons.css";                                //icons
 function App() {
   const [data, setData] = useState([]);
   const [error, setError] = useState({ type: false, message: "" });
-  const [loading, setLoading] = useState("false");
-
+  const [globalFilterValue2, setGlobalFilterValue2] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [filters2, setFilters2] = useState({
+        'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
+        'title': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        'releaseDate': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        'length': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        'director': { value: null, matchMode: FilterMatchMode.IN },
+        'certification': { value: null, matchMode: FilterMatchMode.EQUALS },
+        'rating': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+  });
+  const directorsName =  [...new Map(data.map(item => [item['director'], item])).values()];
+  
   const getMovieData = async () => {
     try {
-      setLoading(true);
       const { data } = await axios.get('https://skyit-coding-challenge.herokuapp.com/movies');
-      setLoading(false);
       data.map(el => el.rating= ((el.rating/5)* 100).toFixed(2) + "%")
       setData(data)
     } catch (err) {
       setError({type: true, message: err.message})
-      setLoading(false)
     }
   }
   useEffect(() => {
+    
     getMovieData();
   }, []);
 
-  // const dynamicColumns = data.map((movie,i) => {
-  //       return <movie key={movie._id} field={movie.field} header={movie.title} />;
-  //   });
+const renderHeader2 = () => {
+    return (
+      <div className="flex justify-content-end">
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText
+            value={globalFilterValue2}
+            onChange={onGlobalFilterChange2}
+            placeholder="Keyword Search"
+          />
+        </span>
+      </div>
+    );
+};
+  const onGlobalFilterChange2 = (e) => {
+    const value = e.target.value;
+    let _filters2 = { ...filters2 };
+    _filters2["global"].value = value;
+
+    setFilters2(_filters2);
+    setGlobalFilterValue2(value);
+  };
+  
+ //refactor
+  const representativeBodyTemplate = (rowData) => {
+    const directorsName = rowData.director;
+    return <span className="image-text">{directorsName}</span>
+  };
+
+  const representativesItemTemplate = (option) => {
+    return (
+      <div className="p-multiselect-representative-option">
+        <span className="image-text">{option.director}</span>
+      </div>
+    );
+  };
+
+  const representativeRowFilterTemplate = (options) => {
+        return <MultiSelect value={options.value} options={directorsName} itemTemplate={representativesItemTemplate}  onChange={(e) => options.filterApplyCallback(e.value)} optionLabel="director" placeholder="ALL" className="p-column-filter" maxSelectedLabels={1} />;
+    }
+
+  const statusBodyTemplate = (rowData) => {
+    return <span className={`customer-badge status-${rowData.status}`}>{rowData.certification}</span>;
+  };
+
+  const statusItemTemplate = (option) => {
+    return <span className={`customer-badge status-${option}`}>{option}</span>;
+  };
+
+ const statusRowFilterTemplate = (options) => {
+        return <Dropdown value={options.value} options={data.certification} onChange={(e) => options.filterApplyCallback(e.value)} placeholder="Select a Status" className="p-column-filter" showClear />;
+    }
+  
   
   return (
     <div>
       <DataTable
         value={data}
         responsiveLayout="scroll"
-        dataKey="_id"
+        className="p-datatable-customers"
+        dataKey="id"
+        filters={filters2}
+        filterDisplay="row"
+        globalFilterFields={['title', 'releaseDate', 'director', 'certification', 'rating']}
+        emptyMessage="No customers found."
+
       >
-        {/* <RadioButton value="val1" name="city"  /> */}
-        <Column selectionMode="single" headerStyle={{ width: '3rem' }} exportable={false}></Column>
-        <Column field="title" header="Title"></Column>
-        <Column field='releaseDate' header="Year"></Column>
-        <Column field='length' header="RunningTime"></Column>
-        <Column field='director' header="Director"></Column>
-        <Column field='certification' header="Certification"></Column>
-        <Column field='rating' header="Rating"></Column>
+        <Column selectionMode="single" ></Column>
+        <Column field="title" header="Title" filter showFilterMenu={false}
+            filterPlaceholder="Search by title" style={{ minWidth: '12rem' }}></Column>
+        <Column field="releaseDate" header="Year" filterField="releaseDate" style={{ minWidth: '12rem' }} filter showFilterMenu={false} filterPlaceholder="Search by release year"></Column>
+        <Column field="length" header="RunningTime" filterField="length" showFilterMenu={false} style={{ minWidth: '12rem' }} filter filterPlaceholder="Search by movie length"></Column>
+        <Column header="director"
+            field="director"
+            filterField="director"
+            showFilterMenu={false}
+            filterMenuStyle={{ width: "14rem" }}
+            style={{ minWidth: "14rem" }}
+            body={representativeBodyTemplate}
+            filter
+            filterElement={representativeRowFilterTemplate}>
+        </Column>
+        <Column field='certification' header="Certification" filterField='certification' showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '12rem' }} body={statusBodyTemplate} filter filterElement={statusRowFilterTemplate}></Column>
+        <Column field='rating' header="Rating" filterField="rating" showFilterMenu={false} style={{ minWidth: '12rem' }} filter filterPlaceholder="Search by movie length"></Column>
       </DataTable>
 
     </div>
